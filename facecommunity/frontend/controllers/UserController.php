13fada19e171df3bc4f8fb27a7f1919dc58ecea0
Alsw <861868603@qq.com>
2017-03-30 20:31:37 +0800

    for help

--- facecommunity/frontend/controllers/UserController.php
+++ facecommunity/frontend/controllers/UserController.php
@@ -3,6 +3,16 @@
 namespace frontend\controllers;
 
 use Yii;
+use yii\base\InvalidParamException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use common\models\LoginForm;
+use frontend\models\PasswordResetRequestForm;
+use frontend\models\ResetPasswordForm;
+use frontend\models\SignupForm;
+use frontend\models\ContactForm;
 
 class UserController extends Controller
 {
@@ -11,4 +21,13 @@ class UserController extends Controller
         return $this->render('index');
     }
 
+    public function actionLogin()
+    {
+    	return $this->render('login.html');
+    }
+
+    public function actionRegister()
+    {
+    	return $this ->render('register');
+    }
 }
---
+++ facecommunity/frontend/views/user/register.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2017-03-30 20:21:00
+ * @version $Id$
+ */
+
+class ClassName extends AnotherClass {
+    
+    function __construct(){
+        
+    }
+}
