e5069f53d274e71d2fb228ae36c97a7dd9f7d157
Alsw <861868603@qq.com>
2017-04-12 10:15:03 +0800

    change birthday database

---
+++ console/migrations/m170411_072958_birthday_change.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m170411_072958_birthday_change extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m170411_072958_birthday_change cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
